cb6a6eba2042ad6cc1f66009b7ef2a0ed52ae0e5
amodi <amitmodi@yahoo.com>
2012-04-05 00:45:48 +0530

    first cut of MR

---
+++ mr-exp/src/main/java/com/amit/exp/mr/ReverseIndexMapper.java
@@ -0,0 +1,29 @@
+package com.amit.exp.mr;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ReverseIndexMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+{
+
+	private final static IntWritable ONE = new IntWritable(1);
+    private final Text WORD = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException 
+    {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) 
+        {
+            WORD.set(itr.nextToken());
+            context.write(WORD, ONE);
+        }
+    }
+
+}
---
+++ mr-exp/src/main/java/com/amit/exp/mr/WordCount.java
@@ -0,0 +1,34 @@
+package com.amit.exp.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class WordCount {
+	
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: hadoop jar <> com.amit.exp.mr.WordCount <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+
+}
